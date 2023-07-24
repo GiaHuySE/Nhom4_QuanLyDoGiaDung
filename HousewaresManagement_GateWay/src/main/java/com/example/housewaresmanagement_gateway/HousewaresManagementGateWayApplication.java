a51b0f8877eb83dd9b4abc816ebc74f72284a76e
Huy Tong <tonghuy214@gmail.com>
2023-07-24 17:22:55 +0700

    add gateway

---
+++ HousewaresManagement_GateWay/src/main/java/com/example/housewaresmanagement_gateway/HousewaresManagementGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.housewaresmanagement_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousewaresManagementGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousewaresManagementGateWayApplication.class, args);
+    }
+
+}
