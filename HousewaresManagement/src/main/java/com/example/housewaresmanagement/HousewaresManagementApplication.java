3b4c90edb5223dbed444724edfa102280b65c0c9
Huy Tong <tonghuy214@gmail.com>
2023-07-24 22:21:23 +0700

    add HouseWares Management project

---
+++ HousewaresManagement/src/main/java/com/example/housewaresmanagement/HousewaresManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.housewaresmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousewaresManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousewaresManagementApplication.class, args);
+    }
+
+}
